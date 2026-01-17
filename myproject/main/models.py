6e5c2ca6ec55b0e636539b58367c3d5a70f9bb71
VeRoyter <VeRoyter@yandex.com>
2026-01-17 18:21:30 +1000

    ManyToManyField corrected

--- myproject/main/models.py
+++ myproject/main/models.py
@@ -36,51 +36,20 @@ class Actor(models.Model):
 class Show(models.Model):
     show_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
-    type = models.CharField(max_length=255)  # Movie или TV Show
+    type = models.CharField(max_length=255)
     date_added = models.DateField(null=True, blank=True)
     release_year = models.IntegerField()
     rating = models.CharField(max_length=50, null=True, blank=True)
     duration = models.CharField(max_length=50, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
 
+    # Добавил ManyToManyField вместо отдельных таблиц
+    countries = models.ManyToManyField(Country, related_name='shows', db_table='Countries Cast')
+    directors = models.ManyToManyField(Director, related_name='shows', db_table='Directors Cast')
+    actors = models.ManyToManyField(Actor, related_name='shows', db_table='Actors Cast')
+
     def __str__(self):
         return self.title
 
     class Meta:
         db_table = 'Shows'
-
-
-class CountriesCast(models.Model):
-    show = models.ForeignKey(Show, on_delete=models.CASCADE, db_column='show_id')
-    country = models.ForeignKey(Country, on_delete=models.CASCADE, db_column='country_id')
-
-    def __str__(self):
-        return f"{self.show.title} - {self.country.country_name}"
-
-    class Meta:
-        db_table = 'Countries Cast'
-        unique_together = ('show', 'country')
-
-
-class DirectorsCast(models.Model):
-    show = models.ForeignKey(Show, on_delete=models.CASCADE, db_column='show_id')
-    director = models.ForeignKey(Director, on_delete=models.CASCADE, db_column='director_id')
-
-    def __str__(self):
-        return f"{self.show.title} - {self.director.director_name}"
-
-    class Meta:
-        db_table = 'Directors Cast'
-        unique_together = ('show', 'director')
-
-
-class ActorsCast(models.Model):
-    show = models.ForeignKey(Show, on_delete=models.CASCADE, db_column='show_id')
-    actor = models.ForeignKey(Actor, on_delete=models.CASCADE, db_column='actor_id')
-
-    def __str__(self):
-        return f"{self.show.title} - {self.actor.actor_name}"
-
-    class Meta:
-        db_table = 'Actors Cast'
-        unique_together = ('show', 'actor')
